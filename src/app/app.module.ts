aed8f532d6e58297cccb8277886a46d20fe86f38
Viktoriia <krjuchenkova@gmail.com>
2022-06-14 14:58:08 +0300

    add previuos markup for main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StackComponent } from './components/stack/stack.component';
+import { QueueComponent } from './components/queue/queue.component';
+import { WebApiComponent } from './components/web-api/web-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StackComponent,
+    QueueComponent,
+    WebApiComponent
   ],
   imports: [
     BrowserModule
